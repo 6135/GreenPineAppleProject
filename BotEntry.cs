97adceb89d1777d6411c0e6deae868132eadef96
Gui Costa <guibscosta@hotmail.com>
2024-12-06 14:56:57 +0000

    Add project files.

---
+++ BotEntry.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace GreenPineAppleProject
+{
+    public class BotEntry
+    {
+        public static async Task Main()
+        {
+        }
+    }
+}
