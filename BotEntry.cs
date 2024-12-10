9ab019ebc4ce84a91c5752c43f6cddd5941247a4
Gui Costa <guibscosta@hotmail.com>
2024-12-10 23:57:51 +0000

    Baseline

--- BotEntry.cs
+++ BotEntry.cs
@@ -1,4 +1,4 @@
-﻿using GreenPineAppleProject.main.src.body;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Sentry;
 
@@ -6,12 +6,15 @@ namespace GreenPineAppleProject
 {
     public class BotEntry
     {
+        public static IConfigurationRoot config;
+
         public static async Task Main()
         {
-            Sentry();
-            var root = Directory.GetCurrentDirectory();
-            var dotenv = Path.Combine(root, ".env");
-            DotEnv.Load(dotenv);
+            //load config
+            config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .Build();
+            await Sentry();
         }
 
         private static async Task Sentry()
@@ -21,13 +24,13 @@ namespace GreenPineAppleProject
                 // A Sentry Data Source Name (DSN) is required.
                 // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
                 // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
-                options.Dsn = "https://3cf5074918c6faaea6f1a8feb7c8fcb4@o4508423730298880.ingest.de.sentry.io/4508423737311312";
+                options.Dsn = config["SentrySettings:Dsn"];
 
                 // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
                 // This might be helpful, or might interfere with the normal operation of your application.
                 // We enable it here for demonstration purposes when first trying Sentry.
                 // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
-                options.Debug = true;
+                options.Debug = Boolean.Parse(config["SentrySettings:Debug"] ?? "false");
 
                 // This option is recommended. It enables Sentry's "Release Health" feature.
                 options.AutoSessionTracking = true;
@@ -51,7 +54,7 @@ namespace GreenPineAppleProject
                     TimeSpan.FromMilliseconds(500)
                 ));
             });
-            SentrySdk.CaptureMessage("Something went wrong");
+            SentrySdk.CaptureMessage("It works!");
         }
     }
 }
